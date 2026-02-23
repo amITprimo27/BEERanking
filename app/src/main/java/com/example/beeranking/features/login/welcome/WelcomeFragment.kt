2b699bbbfed2cea3d2def9742cba0a0242f6cac9
niv cohen <nivnuv008@gmail.com>
2026-02-23 20:43:01 +0200

    finish welcome page

--- app/src/main/java/com/example/beeranking/features/login/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/example/beeranking/features/login/welcome/WelcomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.beeranking.R
 import com.example.beeranking.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -17,5 +19,17 @@ class WelcomeFragment : Fragment() {
         this.binding = FragmentWelcomeBinding.inflate(layoutInflater, container, false)
 
         return this.binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.signUpButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_welcome_to_signup)
+        }
+
+        binding?.signInButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_welcome_to_login)
+        }
     }
 }
