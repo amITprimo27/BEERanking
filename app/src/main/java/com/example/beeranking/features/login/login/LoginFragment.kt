9f6855d48172bb43146a700da78bcdbf0b6c2777
niv cohen <nivnuv008@gmail.com>
2026-02-24 18:45:00 +0200

    start of login page

--- app/src/main/java/com/example/beeranking/features/login/login/LoginFragment.kt
+++ app/src/main/java/com/example/beeranking/features/login/login/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.beeranking.R
 import com.example.beeranking.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -17,5 +19,13 @@ class LoginFragment : Fragment() {
         this.binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
 
         return this.binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.signupTextView?.setOnClickListener {
+            findNavController().navigate(R.id.action_login_to_signup)
+        }
     }
 }
