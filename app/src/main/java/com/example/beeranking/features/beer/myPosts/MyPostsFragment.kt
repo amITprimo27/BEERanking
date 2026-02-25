0c880b64276b30495da447ddd6fb8f1969d2e081
niv cohen <nivnuv008@gmail.com>
2026-02-25 14:44:14 +0200

    fix after cr

--- app/src/main/java/com/example/beeranking/features/beer/myPosts/MyPostsFragment.kt
+++ app/src/main/java/com/example/beeranking/features/beer/myPosts/MyPostsFragment.kt
@@ -1,11 +1,10 @@
-package com.example.beeranking.features.beer.myposts
+package com.example.beeranking.features.beer.myPosts
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.beeranking.R
+import androidx.fragment.app.Fragment
 import com.example.beeranking.databinding.FragmentMyPostsBinding
 
 class MyPostsFragment : Fragment() {
